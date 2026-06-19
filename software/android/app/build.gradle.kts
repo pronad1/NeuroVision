e84ebdf503a5ad4950ec8be23baf0c2f2ddcadba
pronad1 <ug2102049@cse.pstu.ac.bd>
2026-06-19 23:41:41 +0600

    android

--- software/android/app/build.gradle.kts
+++ software/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = flutter.compileSdkVersion
 
     // Set NDK version to match the highest version required by plugins
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
